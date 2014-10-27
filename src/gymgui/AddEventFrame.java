04e95480a74ffcf38def40f3f33063d49ad46581
DMitryRak <rakdimka@rambler.ru>
2014-10-27 23:33:00 +0300

    + some additional classes

--- src/gymgui/AddEventFrame.java
+++ src/gymgui/AddEventFrame.java
@@ -2,6 +2,7 @@ package gymgui;
 
 /**
  * Created by dmitry on 10/27/14.
+ * Will be used to add new event. Will be invoked from EventFrame
  */
 public class AddEventFrame {
 }
---
+++ src/gymgui/MainFrame.java
@@ -0,0 +1,8 @@
+package gymgui;
+
+/**
+ * Created by dmitry on 10/27/14.
+ * Both calendar and event frame will be connected to MainFrame and can be switched from one to another by JTabbedPane
+ */
+public class MainFrame {
+}
