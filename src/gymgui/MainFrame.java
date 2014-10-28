2e197dde984367f98c5efa979701d64370926df8
DMitryRak <rakdimka@rambler.ru>
2014-10-28 23:34:12 +0300

    + GUI v.1.00

--- src/gymgui/MainFrame.java
+++ src/gymgui/MainFrame.java
@@ -1,8 +1,30 @@
 package gymgui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * Created by dmitry on 10/27/14.
- * Both calendar and event frame will be connected to MainFrame and can be switched from one to another by JTabbedPane
+ * Created by dmitry on 10/28/14.
  */
 public class MainFrame {
+    private JTabbedPane tabbedPane1;
+    private JPanel EventPane;
+    private JList eventList;
+    private JButton addNewEventButton;
+    private JPanel eventDetails;
+    private JScrollPane eventScroller;
+    private JComboBox years;
+    private JComboBox monthes;
+    private JPanel daysPane;
+
+    public MainFrame(){
+        JFrame frame = new JFrame("GymTracker");
+        frame.setSize(new Dimension(1000,1000));
+        frame.setContentPane(EventPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultLookAndFeelDecorated(true);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- src/test.java
+++ src/test.java
@@ -1,8 +1,11 @@
+import gymgui.MainFrame;
+
 /**
  * Created by dmitry on 10/27/14.
  */
 public class test {
     public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
 
     }
 }
